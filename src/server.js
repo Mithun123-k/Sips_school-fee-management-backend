8f7de080a545ba8b732e9c779f355bcf3237c399
Mithun Kumar <mithunkumar2810@gmail.com>
2026-07-30 14:01:41 +0530

    server setup done

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = require("./app");
 const connectDB = require("./config/db");
 const createDefaultAdmin = require("./config/createAdmin");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
@@ -13,7 +13,7 @@ const startServer = async () => {
 
     await createDefaultAdmin();
 
-    app.listen(PORT, () => {
+   app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server Running On Port ${PORT}`);
     });
   } catch (error) {
